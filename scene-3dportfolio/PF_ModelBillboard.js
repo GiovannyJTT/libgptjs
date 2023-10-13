0278cdfbf9958561775d3cc771d2b52ccfee0a5c
Giovanny Javier Tipantuña Toapanta <67277278+GiovannyJTT@users.noreply.github.com>
2023-10-13 17:59:35 +0200

    .\PF_ModelBillboard.js recentering the object when loaded-finished, calc_pos_per_wp_country() at the beginning and storing the positions to update later

--- scene-3dportfolio/PF_ModelBillboard.js
+++ scene-3dportfolio/PF_ModelBillboard.js
@@ -56,6 +56,7 @@ PF_ModelBillboard.prototype.load_obj = function (mats_) {
 
         function on_loaded_sequence (obj_) {
             this.adapt_to_scene(obj_);
+            this.calc_pos_per_wp_country();
 
             // 3. call external callback to add model to scene
             this.on_loaded_external_cb.call(this, this.billboard_obj);
@@ -72,11 +73,19 @@ PF_ModelBillboard.prototype.load_obj = function (mats_) {
 }
 
 PF_ModelBillboard.prototype.adapt_to_scene = function (obj_) {
-    this.billboard_obj = obj_;    
+    this.billboard_obj = obj_;
+
+    // recenter object
+    for (let i=0; i < this.billboard_obj.children.length; i++) {
+        let c = this.billboard_obj.children[i];
+        c.geometry.computeBoundingBox();
+        c.geometry.center();
+    }
+
     this.billboard_obj.scale.set(PF_Common.BILLBOARD_SCALE, PF_Common.BILLBOARD_SCALE, PF_Common.BILLBOARD_SCALE);
     
     // initially at origin, it will be moved to its position when first update()
-    this.billboard_obj.position.set(0,0,0);
+    this.billboard_obj.position.set(0, 0, 0);
 
     const _bb = new THREE.Box3().setFromObject(this.billboard_obj);
     this.size = new THREE.Vector3();
@@ -85,39 +94,70 @@ PF_ModelBillboard.prototype.adapt_to_scene = function (obj_) {
 }
 
 /**
- * - Places the billboard-object at a point between current and next waypoint-country in order to show
- * information on the panel while the drone is reaching the next target-waypoint
- * @param {Dictionary} wp_segment Example:
+ * - Calculates the position of the billboard-object in the middle between the previous and the current
+ *  wp-country-coordinates and stores them into `this.pos_per_wp`
+ * - Example of a segment formed by two waypoint-contry:
+ * Example:
  * ```json
  * {
  *  wp_start: {name: "VALENCIA", coords: {x: -204.4, y: 366.6}, date: "2017-March", wp_index: 0},
  *  wp_end: {name: "HELSINKI", coords: {x: 343.3, y: -244.4}, date: "2019-March", wp_index: 1}
  * }
  * ```
+ * - NOTE: It adds an artificial init-point at the beginning of the wp-list in order to
+ *  place properly the first billboard for the first contry with a proper displacement to be focused by
+ *  the follow-camera while landing:
+ * ```json
+ *  {name: "INIT", coords: {x: -900, y: 1200}, date: "0000-00-00", wp_index: -1}
+ * ```
  */
-PF_ModelBillboard.prototype.place_at_wp = function (wp_segment) {
+PF_ModelBillboard.prototype.calc_pos_per_wp_country = function () {
+    if (PF_Common.FPATH_WPS.length == 0) {
+        console.error("FPATH_WPS is empty");
+        return;
+    }
+
+    let wps = PF_Common.FPATH_WPS;
+    wps.unshift(
+        {name: "INIT", coords: {x: -900, y: 1200}, date: "0000-00-00", wp_index: -1}
+    );
+
+    this.pos_per_wp = [];
+
+    for (let i=1; i < wps.length; i++) {
+        const wp_end = wps[i];
+        const wp_start = wps[i-1];
+
+        const end = new THREE.Vector3(wp_end.coords.x, 0, wp_end.coords.y);
+        const start = new THREE.Vector3(wp_start.coords.x, 0, wp_start.coords.y);
+        // displacement based on direction-vector to not overlap with drone
+
+        const disp = new THREE.Vector3().copy(end)
+            .sub(start).normalize().multiplyScalar(2.5 * this.size.z);
+
+        const end_pos = new THREE.Vector3(
+            end.x,
+            this.size.y / 2,
+            end.z
+        );
+
+        const final_pos = end_pos.sub(disp);
+        this.pos_per_wp.push(final_pos);
+    }
+}
+
+/**
+ * - Places the billboard-object at a point between current and next waypoint-country in order to show
+ * information on the panel while the drone is reaching the next target-waypoint
+ * @param {Dictionary} wp_segment 
+ */
+PF_ModelBillboard.prototype.place_at_wp = function (wp_index) {
     if (undefined === this.billboard_obj){
         return;
     }
 
-    const end = new THREE.Vector3(wp_segment.wp_end.coords.x, 0, wp_segment.wp_end.coords.y);
-    const start = new THREE.Vector3(wp_segment.wp_start.coords.x, 0, wp_segment.wp_start.coords.y);
-
-    const end_pos = new THREE.Vector3(
-        end.x,
-        // this model is not centered properly, fixing coord 'y'
-        (this.size.y / 2) - (this.size.y * 0.075),
-        end.z
-    );
-
-    // displacement based on wp-segment direction to not collide with drone
-    const v = end.sub(start).normalize();
-    const disp = v.multiplyScalar(2.5 * this.size.z);
-    const disp_pos = end_pos.sub(disp);
-
-    // update pos
-    const final_pos = disp_pos;
-    this.billboard_obj.position.set(final_pos.x, final_pos.y, final_pos.z);
+    const pos = this.pos_per_wp[wp_index];
+    this.billboard_obj.position.set(pos.x, pos.y, pos.z);
 }
 
 PF_ModelBillboard.prototype.face_to = function (lookat_pos) {
