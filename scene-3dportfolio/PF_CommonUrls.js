07e0c10b46b81a18240124fa04dc1ca589fb82d8
Giovanny Javier Tipantuña Toapanta <67277278+GiovannyJTT@users.noreply.github.com>
2023-10-23 21:07:11 +0200

    .\PF_CommonUrls.js added .jpg extension

--- scene-3dportfolio/PF_CommonUrls.js
+++ scene-3dportfolio/PF_CommonUrls.js
@@ -12,7 +12,7 @@ Object.entries(FOLDER_PICTURES).forEach(
     ([k, v]) => {
         let city_img_paths = [];
         for (let i=0; i < 10; i++) {
-            city_img_paths.push( base_path + k + "/img_" + i.toString().padStart(3, "0") );
+            city_img_paths.push( base_path + k + "/img_" + i.toString().padStart(3, "0") + ".jpg");
         }
         FOLDER_PICTURES[k] = city_img_paths;
     }
